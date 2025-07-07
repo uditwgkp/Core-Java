5252f258e5e4a2327a13b91ab4c741ed743077ee
uditwgkp <uditwgkp123@gmail.com>
2025-07-07 12:32:12 +0530

    Star Pattern in Java

--- src/Java/Pattern.java
+++ src/Java/Pattern.java
@@ -103,5 +103,40 @@ public class Pattern {
             }
             System.out.println();
         }
+
+        /*------ PYRAMID------ */
+        System.out.println("Pyramid");
+        for (int i = 1; i <= pyramidHeight; i++) {
+            for (int j = 1; j <= pyramidHeight-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+
+        /*------PALINDROMIC NUMBER PYRAMID------- */
+        System.out.println("Palindromic number pyramid");
+        for (int i = 1; i <= pyramidHeight; i++) {
+             for (int j = 1; j <= pyramidHeight-i; j++) {
+                System.out.print(" ");
+             }
+             int num = i;
+             for (int j = 1; j <= (2*i-1 ); j++) {
+               
+                if (num==1) {
+                    System.out.print("1");
+                    num++;
+                }else if (num>1){
+                    System.out.print(num);
+                    num--;
+                }else{
+                    System.out.print(num);
+                    num++;
+                }
+             }
+             System.out.println();
+        }
     }
 }
